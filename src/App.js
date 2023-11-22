d0bb02eb9149c8d2492c661fb70bca9e9b3a6d9e
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-11-22 19:22:01 +0100

    update for correct answer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [gameOver, setGameOver] = useState(false);
   const [questionCount, setQuestionCount] = useState(0);
   const [moneyEarned, setMoneyEarned] = useState(0);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
 
   const maxQuestions = 15;
   const prices = [100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000];
@@ -42,12 +43,17 @@ function App() {
   const handleAnswer = (selectedAnswer) => {
     if (selectedAnswer === questions[currentQuestionIndex].correct_answer) {
       setMoneyEarned(prices[questionCount]);
-      handleNextQuestion();
+      setIsAnswerCorrect(true);
+  
+      setTimeout(() => {
+        setIsAnswerCorrect(false);
+        handleNextQuestion();
+      }, 1000);
     } else {
       setGameOver(true);
     }
   };  
-  
+
   const handleNextQuestion = () => {
     if (currentQuestionIndex + 1 < maxQuestions && questionCount + 1 < maxQuestions) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
@@ -57,7 +63,7 @@ function App() {
       setGameOver(true);
     }
   };
-  
+
   const handleRestartGame = () => {
     setMoneyEarned(0);
     setQuestions(shuffleQuestions(questions));
@@ -71,57 +77,66 @@ function App() {
       <h1>Who Wants to Be a Millionaire?</h1>
       <div className="container">
         <div className="game">
-        {currentQuestionIndex < questions.length && !gameOver && (
-          <>
-            <button onClick={() => setGameOver(true)}>Give up!</button>
-            <div className="question">
-              <h2> {questions[currentQuestionIndex].question} </h2>
-            </div>
+          {currentQuestionIndex < questions.length && !gameOver && (
+            <>
+              <button onClick={() => setGameOver(true)}>Give up!</button>
+              <div className="question">
+                <h2> {questions[currentQuestionIndex].question} </h2>
+              </div>
 
-            <div className="answers">
-              {questions[currentQuestionIndex].type === "multiple" &&
-                questions[currentQuestionIndex].incorrect_answers.map((answer, index) => (
-                  <div key={index}>
-                    <button onClick={() => handleAnswer(answer)}>{answer}</button>
+              <div className="answers">
+                {questions[currentQuestionIndex].type === "multiple" &&
+                  questions[currentQuestionIndex].incorrect_answers.map((answer, index) => (
+                    <div key={index}>
+                      <button
+                        onClick={() => handleAnswer(answer)}
+                        className={isAnswerCorrect && answer === questions[currentQuestionIndex].correct_answer ? 'correct-answer' : ''}
+                      >
+                        {answer}
+                      </button>
+                    </div>
+                  ))}
+                {questions[currentQuestionIndex].type === "multiple" && (
+                  <div>
+                    <button
+                      onClick={() => handleAnswer(questions[currentQuestionIndex].correct_answer)}
+                      className={isAnswerCorrect ? 'correct-answer' : ''}
+                    >
+                      {questions[currentQuestionIndex].correct_answer}
+                    </button>
                   </div>
-                ))}
-              {questions[currentQuestionIndex].type === "multiple" && (
-                <div>
-                  <button onClick={() => handleAnswer(questions[currentQuestionIndex].correct_answer)}>
-                    {questions[currentQuestionIndex].correct_answer}
-                  </button>
-                </div>
-              )}
-            </div>
-          </>
-        )}
+                )}
+              </div>
 
-        {gameOver && (
-          <>
-            <p>Game Over!</p>
-            <p>You won: ${moneyEarned}</p>
-            <button onClick={handleRestartGame}>Start again!</button>
-          </>
-        )}
+            </>
+          )}
+
+          {gameOver && (
+            <>
+              <p>Game Over!</p>
+              <p>You won: ${moneyEarned}</p>
+              <button onClick={handleRestartGame}>Start again!</button>
+            </>
+          )}
         </div>
         <div className="prizes">
-        <table>
-          <thead>
-            <tr>
-              <th>Question</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {prices.slice().reverse().map((price, index) => (
-              <tr key={index} style={{ backgroundColor: currentQuestionIndex + 1 === maxQuestions - index ? '#D6640F' : 'inherit' }}>
-                <td>{maxQuestions - index}</td>
-                <td>{maxQuestions - index === 1 ? `${price}€` : `${price}$`}</td>
+          <table>
+            <thead>
+              <tr>
+                <th>Question</th>
+                <th>Price</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {prices.slice().reverse().map((price, index) => (
+                <tr key={index} style={{ backgroundColor: currentQuestionIndex + 1 === maxQuestions - index ? '#D6640F' : 'inherit' }}>
+                  <td>{maxQuestions - index}</td>
+                  <td>{maxQuestions - index === 1 ? `${price}€` : `${price}$`}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
